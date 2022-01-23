613358ea952af55a83884ce65abe9324939f2add
Chris Eicher <chrisaeicher@gmail.com>
2022-01-22 17:55:56 -0800

    Fixed silly misspelling.

--- nextjs-blog/components/date.js
+++ nextjs-blog/components/date.js
@@ -2,5 +2,5 @@ import { parseISO, format } from "date-fns";
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString);
-  return <time dataTime={dateString}>{format(date, "LLLL d, yyy")}</time>;
+  return <time dateTime={dateString}>{format(date, "LLLL d, yyy")}</time>;
 }
